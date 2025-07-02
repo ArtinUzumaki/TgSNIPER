c0589f6fb3aa2aa22ec910d367d17bd3fb025dea
Artin Uzumaki <konohaArtin@gmail.com>
2025-07-03 02:17:10 +0430

    update config.py TGSniper v1.1

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 # مقدار API_ID و API_HASH رو از https://my.telegram.org دریافت کن
 
-API_ID = 1234567
-API_HASH = "abcdefghiklmnop1686195"
+API_ID = ENTER YOUR API-ID
+API_HASH = "enter your API-HASH"
 SESSION_NAME = "tg_sniper_session"
 LOG_FILE = "tg_sniper.log"
