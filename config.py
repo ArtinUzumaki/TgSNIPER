669edcacfbc35c59da287b29696eb8337ed68201
Artin Uzumaki <konohaArtin@gmail.com>
2025-07-03 17:13:00 +0430

    Update config.py 1.1v

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 # مقدار API_ID و API_HASH رو از https://my.telegram.org دریافت کن
 
-API_ID = ENTER YOUR API-ID
+API_ID = enter your API-ID
 API_HASH = "enter your API-HASH"
 SESSION_NAME = "tg_sniper_session"
 LOG_FILE = "tg_sniper.log"
